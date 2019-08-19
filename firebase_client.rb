8389d76e0de408d6536804d97922f790504824bb
Kyohei Sakai <ladnack@gmail.com>
2019-08-19 13:52:56 +0900

    Add comment about authentication method for firebase

--- firebase_client.rb
+++ firebase_client.rb
@@ -4,10 +4,14 @@ require 'firebase'
 
 class FirebaseClient
   def initialize
-    # Using Firebase Database Secret (deprecated)
+    # 1. Using Firebase Database Secret (deprecated)
     @firebase = Firebase::Client.new(ENV['FIREBASE_URL'], ENV['FIREBASE_SECRET'])
 
-    # Using Firebase Admin SDK private key
+    # NOTE: We use Firebase Database Secret above rather than Admin SDK private
+    # key below, because we have no idea how we manage the private key (a json
+    # file) on Heroku.
+    #
+    # 2. Using Firebase Admin SDK private key
     # private_key_json_string = File.open('firebase-adminsdk-fz5jd-c6ec071feb.json').read
     # @firebase = Firebase::Client.new(ENV['FIREBASE_URL'], private_key_json_string)
   end
